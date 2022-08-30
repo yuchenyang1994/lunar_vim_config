aa8bf708bfe5c0b3c28742085c9890d5a457e6dc
remy.yu <remy.yu@ucloud.cn>
2022-08-30 11:57:32 +0800

    lunavim config

---
+++ config.lua
@@ -0,0 +1,463 @@
+--[[
+lvim is the global options object
+
+Linters should be
+filled in as strings with either
+a global executable or a path to
+an executable
+]]
+-- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+
+lvim.plugins = {
+  {
+    "TimUntersberger/neogit",
+    cmd = "Neogit",
+    opt = true,
+    config = function()
+      require("module.neogit")
+    end,
+  },
+  {
+    'phaazon/hop.nvim',
+    as = 'hop',
+    config = function()
+      require('hop').setup()
+    end
+  },
+  {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+}
+
+-- general
+lvim.log.level = "warn"
+lvim.format_on_save = true
+lvim.colorscheme = "onedarker"
+-- to disable icons and use a minimalist setup, uncomment the following
+-- lvim.use_icons = false
+
+-- keymappings [view all the defaults by pressing <leader>Lk]
+lvim.leader = "space"
+-- add your own keymapping
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+-- unmap a default keymapping
+-- vim.keymap.del("n", "<C-Up>")
+-- override a default keymapping
+-- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
+
+-- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
+-- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
+-- local _, actions = pcall(require, "telescope.actions")
+-- lvim.builtin.telescope.defaults.mappings = {
+--   -- for input mode
+--   i = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--     ["<C-n>"] = actions.cycle_history_next,
+--     ["<C-p>"] = actions.cycle_history_prev,
+--   },
+--   -- for normal mode
+--   n = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--   },
+-- }
+
+-- Use which-key to add extra bindings with the leader-key prefix
+-- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+-- lvim.builtin.which_key.mappings["t"] = {
+--   name = "+Trouble",
+--   r = { "<cmd>Trouble lsp_references<cr>", "References" },
+--   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+--   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+--   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+--   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+--   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
+-- }
+
+lvim.builtin.which_key.vmappings = {
+  [";"] = {
+    name = "Comment",
+    [";"] = { "<Plug>(comment_toggle_linewise_visual)", "Comment toggle linewise (visual)" }
+  }
+}
+
+lvim.builtin.which_key.mappings = {
+  f = {
+    name = "File",
+    s = { "<cmd>w!<CR>", "Save" },
+    f = { require("lvim.core.telescope.custom-finders").find_project_files, "Find File" },
+  },
+  h = {
+    name = "Highlights",
+    l = { "<cmd>nohlsearch<CR>", "Clear Highlight" }
+  },
+  b = {
+    name = "Buffers",
+    j = { "<cmd>BufferLinePick<cr>", "Jump" },
+    f = { "<cmd>Telescope buffers<cr>", "Find" },
+    b = { "<cmd>BufferLineCyclePrev<cr>", "Previous" },
+    n = { "<cmd>BufferLineCycleNext<cr>", "Next" },
+    -- w = { "<cmd>BufferWipeout<cr>", "Wipeout" }, -- TODO: implement this for bufferline
+    e = {
+      "<cmd>BufferLinePickClose<cr>",
+      "Pick which buffer to close",
+    },
+    h = { "<cmd>BufferLineCloseLeft<cr>", "Close all to the left" },
+    l = {
+      "<cmd>BufferLineCloseRight<cr>",
+      "Close all to the right",
+    },
+    D = {
+      "<cmd>BufferLineSortByDirectory<cr>",
+      "Sort by directory",
+    },
+    L = {
+      "<cmd>BufferLineSortByExtension<cr>",
+      "Sort by language",
+    },
+  },
+
+  ["<tab>"] = {
+    name = "Workspace",
+    ["<tab>"] = { "<cmd>tabnew<CR>", "New Tab" },
+
+    n = { "<cmd>tabnext<CR>", "Next" },
+    d = { "<cmd>tabclose<CR>", "Close" },
+    p = { "<cmd>tabprevious<CR>", "Previous" },
+    f = { "<cmd>tabfirst<CR>", "First" },
+    l = { "<cmd>tablast<CR>", "Last" },
+  },
+
+  g = {
+    name = "Git",
+    j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
+    k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
+    B = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+    p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+    r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
+    R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+    S = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+    s = { "<cmd>Neogit kind=split<CR>", "Git Status" },
+    u = {
+      "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
+      "Undo Stage Hunk",
+    },
+    o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
+    b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+    c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+    C = {
+      "<cmd>Telescope git_bcommits<cr>",
+      "Checkout commit(for current file)",
+    },
+    d = {
+      "<cmd>Gitsigns diffthis HEAD<cr>",
+      "Git Diff",
+    },
+  },
+  w = {
+    name = "Window",
+    s = { "<cmd>split<CR>", "Split Window" },
+    v = { "<cmd>vsplit<CR>", "VSplit Window" },
+    d = { "<cmd>clo<CR>", "Kill Window" },
+    h = { "<C-w>h", "Move Left Windows" },
+    j = { "<C-w>j", "Move Down Windows" },
+    k = { "<C-w>k", "Move Up Windows" },
+    l = { "<C-w>l", "Move Right Windows" },
+    J = { "<cmd>resize -4<CR>", "Resize Windows Down" },
+    K = { "<cmd>resize +4<CR>", "Resize Windows Up" },
+    H = { "<cmd>vertical resize -4<CR>", "Resize Window Left" },
+    L = { "<cmd>vertical resize +4<CR>", "Resize Window Right" }
+
+  },
+  q = {
+    name = "Quit",
+    q = { "<cmd>lua require('lvim.utils.functions').smart_quit()<CR>", "Quit" },
+    Q = { "<cmd>:qa!<cr>", "Quit without saving" },
+  },
+  s = {
+    name = "Search",
+    b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+    c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
+    f = { "<cmd>Telescope find_files<cr>", "Find File" },
+    h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
+    H = { "<cmd>Telescope highlights<cr>", "Find highlight groups" },
+    M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
+    r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+    R = { "<cmd>Telescope registers<cr>", "Registers" },
+    t = { "<cmd>Telescope live_grep<cr>", "Text" },
+    k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
+    C = { "<cmd>Telescope commands<cr>", "Commands" },
+    p = {
+      "<cmd>lua require('telescope.builtin').colorscheme({enable_preview = true})<cr>",
+      "Colorscheme with Preview",
+    },
+  },
+  p = {
+    name = "Project",
+    p = { "<cmd>Telescope projects<CR>", "Find Projects" },
+  },
+  c = {
+    name = "Code",
+    a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+    d = { "<cmd>Telescope diagnostics bufnr=0 theme=get_ivy<cr>", "Buffer Diagnostics" },
+    w = { "<cmd>Telescope diagnostics<cr>", "Diagnostics" },
+    f = { require("lvim.lsp.utils").format, "Format" },
+    i = { "<cmd>LspInfo<cr>", "Info" },
+    I = { "<cmd>Mason<cr>", "Mason Info" },
+    j = {
+      vim.diagnostic.goto_next,
+      "Next Diagnostic",
+    },
+    g = {
+      name = "Goto",
+      d = { "<cmd>lua vim.lsp.buf.definition<cr>", "Definition" },
+      D = { "<cmd>Telescope lsp_definitions", "Definitions" },
+      r = { "<cmd>Telescope lsp_references", "References" },
+    },
+    k = {
+      vim.diagnostic.goto_prev,
+      "Prev Diagnostic",
+    },
+    l = { vim.lsp.codelens.run, "CodeLens Action" },
+    p = {
+      name = "Project/Peek",
+      p = { "<cmd>Telescope projects<CR>", "Projects" },
+      d = { "<cmd>lua require('lvim.lsp.peek').Peek('definition')<cr>", "Definition" },
+      t = { "<cmd>lua require('lvim.lsp.peek').Peek('typeDefinition')<cr>", "Type Definition" },
+      i = { "<cmd>lua require('lvim.lsp.peek').Peek('implementation')<cr>", "Implementation" },
+    },
+    q = { vim.diagnostic.setloclist, "Quickfix" },
+    r = { vim.lsp.buf.rename, "Rename" },
+    h = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+    S = {
+      "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
+      "Workspace Symbols",
+    },
+    e = { "<cmd>Telescope quickfix<cr>", "Telescope Quickfix" },
+  },
+  o = {
+    name = "Open",
+    e = { "<cmd>NvimTreeToggle<CR>", "Explorer" },
+    t = {
+      name = "Terminal",
+      t = { "<cmd>ToggleTerm direction=horizontal<CR>", "Open Terminal(horizontal)" },
+      v = { "<cmd>ToggleTerm direction=vertical size=100<CR>", "Open Terminal(vertical)" },
+      f = { "<cmd>ToggleTerm direction=float<CR>", "Open Terminal(float)" }
+    }
+  },
+  L = {
+    name = "+LunarVim",
+    c = {
+      "<cmd>edit " .. get_config_dir() .. "/config.lua<cr>",
+      "Edit config.lua",
+    },
+    f = {
+      "<cmd>lua require('lvim.core.telescope.custom-finders').find_lunarvim_files()<cr>",
+      "Find LunarVim files",
+    },
+    g = {
+      "<cmd>lua require('lvim.core.telescope.custom-finders').grep_lunarvim_files()<cr>",
+      "Grep LunarVim files",
+    },
+    k = { "<cmd>Telescope keymaps<cr>", "View LunarVim's keymappings" },
+    i = {
+      "<cmd>lua require('lvim.core.info').toggle_popup(vim.bo.filetype)<cr>",
+      "Toggle LunarVim Info",
+    },
+    I = {
+      "<cmd>lua require('lvim.core.telescope.custom-finders').view_lunarvim_changelog()<cr>",
+      "View LunarVim's changelog",
+    },
+    l = {
+      name = "+logs",
+      d = {
+        "<cmd>lua require('lvim.core.terminal').toggle_log_view(require('lvim.core.log').get_path())<cr>",
+        "view default log",
+      },
+      D = {
+        "<cmd>lua vim.fn.execute('edit ' .. require('lvim.core.log').get_path())<cr>",
+        "Open the default logfile",
+      },
+      l = {
+        "<cmd>lua require('lvim.core.terminal').toggle_log_view(vim.lsp.get_log_path())<cr>",
+        "view lsp log",
+      },
+      L = { "<cmd>lua vim.fn.execute('edit ' .. vim.lsp.get_log_path())<cr>", "Open the LSP logfile" },
+      n = {
+        "<cmd>lua require('lvim.core.terminal').toggle_log_view(os.getenv('NVIM_LOG_FILE'))<cr>",
+        "view neovim log",
+      },
+      N = { "<cmd>edit $NVIM_LOG_FILE<cr>", "Open the Neovim logfile" },
+      p = {
+        "<cmd>lua require('lvim.core.terminal').toggle_log_view(get_cache_dir() .. '/packer.nvim.log')<cr>",
+        "view packer log",
+      },
+      P = { "<cmd>edit $LUNARVIM_CACHE_DIR/packer.nvim.log<cr>", "Open the Packer logfile" },
+    },
+    n = { "<cmd>Telescope notify<cr>", "View Notifications" },
+    r = { "<cmd>LvimReload<cr>", "Reload LunarVim's configuration" },
+    u = { "<cmd>LvimUpdate<cr>", "Update LunarVim" },
+  },
+  j = {
+    name = "Jump",
+    w = { "<cmd>HopWord<CR>", "Jump World" },
+    l = { "<cmd>HopLine<CR>", "Jump Line" },
+    c = { "<cmd>HopChar1<CR>", "Jump Char1" },
+    C = { "<cmd>HopChar2<CR>", "Jump Char2" },
+  },
+  T = {
+    name = "Treesitter",
+    i = { ":TSConfigInfo<cr>", "Info" },
+  },
+  [";"] = {
+    name = "Comment",
+    [";"] = { "<Plug>(comment_toggle_linewise_current)", "Comment toggle current line" }
+  },
+  e = {
+    name = "Errors",
+    r = { "<cmd>Trouble lsp_references<cr>", "References" },
+    f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+    d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnosticss" },
+    q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+    l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+    w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnosticss" },
+  }
+
+}
+
+-- TODO: User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.notify.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
+
+
+
+-- generic LSP settings
+
+-- -- make sure server will always be installed even if the server is in skipped_servers list
+-- lvim.lsp.installer.setup.ensure_installed = {
+--     "sumeko_lua",
+--     "jsonls",
+-- }
+-- -- change UI setting of `LspInstallInfo`
+-- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
+-- lvim.lsp.installer.setup.ui.check_outdated_servers_on_open = false
+-- lvim.lsp.installer.setup.ui.border = "rounded"
+-- lvim.lsp.installer.setup.ui.keymaps = {
+--     uninstall_server = "d",
+--     toggle_server_expand = "o",
+-- }
+
+-- ---@usage disable automatic installation of servers
+-- lvim.lsp.automatic_servers_installation = false
+
+-- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
+-- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+-- local opts = {} -- check the lspconfig documentation for a list of all possible options
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
+-- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
+-- vim.tbl_map(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
+
+-- -- you can set a custom on_attach function that will be used for all the language servers
+-- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+
+-- -- set a formatter, this will override the language server formatting capabilities (if it exists)
+-- local formatters = require "lvim.lsp.null-ls.formatters"
+-- formatters.setup {
+--   { command = "black", filetypes = { "python" } },
+--   { command = "isort", filetypes = { "python" } },
+--   {
+--     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+--     command = "prettier",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     extra_args = { "--print-with", "100" },
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "typescript", "typescriptreact" },
+--   },
+-- }
+
+-- -- set additional linters
+-- local linters = require "lvim.lsp.null-ls.linters"
+-- linters.setup {
+--   { command = "flake8", filetypes = { "python" } },
+--   {
+--     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+--     command = "shellcheck",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     extra_args = { "--severity", "warning" },
+--   },
+--   {
+--     command = "codespell",
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "javascript", "python" },
+--   },
+-- }
+
+-- Additional Plugins
+-- lvim.plugins = {
+--     {"folke/tokyonight.nvim"},
+--     {
+--       "folke/trouble.nvim",
+--       cmd = "TroubleToggle",
+--     },
+-- }
+
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   pattern = { "*.json", "*.jsonc" },
+--   -- enable wrap mode for json files only
+--   command = "setlocal wrap",
+-- })
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "zsh",
+--   callback = function()
+--     -- let treesitter use bash highlight for zsh files as well
+--     require("nvim-treesitter.highlight").attach(0, "bash")
+--   end,
+-- })
