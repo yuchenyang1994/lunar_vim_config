0c8adfb75d31e3b5fc0f2550fa556f88e7a5350a
remy.yu <remy.yu@ucloud.cn>
2022-09-14 13:18:05 +0800

    Fix: rust-tools

--- config.lua
+++ config.lua
@@ -106,22 +106,31 @@ lvim.plugins = {
   {
     "simrat39/rust-tools.nvim",
     config = function()
-      local lsp_installer_servers = require "nvim-lsp-installer.servers"
-      local _, requested_server = lsp_installer_servers.get_server "rust_analyzer"
-      require("rust-tools").setup({
+      require("rust-tools").setup {
         tools = {
+          reload_workspace_from_cargo_toml = true,
           autoSetHints = true,
-          hover_with_actions = true,
+          inlay_hints = {
+            auto = true,
+            only_current_line = false,
+            show_parameter_hints = true,
+            parameter_hints_prefix = "<-",
+            other_hints_prefix = "=>",
+            max_len_align = false,
+            max_len_align_padding = 1,
+            right_align = false,
+            right_align_padding = 7,
+            highlight = "Comment",
+          },
           runnables = {
             use_telescope = true,
           },
         },
         server = {
-          cmd_env = requested_server._default_options.cmd_env,
-          on_attach = require("lvim.lsp").common_on_attach,
           on_init = require("lvim.lsp").common_on_init,
+          on_attach = require("lvim.lsp").common_on_attach,
         },
-      })
+      }
     end,
     ft = { "rust", "rs" },
   },
