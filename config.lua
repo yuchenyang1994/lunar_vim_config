2620ab23109742055e0caeb6d578acb52490ce85
remy.yu <remy.yu@ucloud.cn>
2022-10-11 16:30:14 +0800

    Fix: lsp_signature

--- config.lua
+++ config.lua
@@ -156,7 +156,7 @@ lvim.plugins = {
   {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
-    config = function() require "lsp_signature".on_attach() end,
+    config = function() require "lsp_signature".setup() end,
   },
   {
     "folke/todo-comments.nvim",
